99dbd408d1657671b1b789aab139922a7ee442cf
VIGNESH-R0208 <vickyvke0208@gmail.com>
2023-06-07 13:12:25 +0530

    about

--- src/components/HomepageDetails/About.js
+++ src/components/HomepageDetails/About.js
@@ -1,9 +1,10 @@
 import Grid from '@mui/material/Grid';
-import { useEffect,useState } from 'react';
-import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-const About=()=>{
+import { useEffect, useState } from 'react';
+
+
+const About = () => {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
-  
+
   useEffect(() => {
     const mediaQuery = window.matchMedia('(max-width: 768px)');
 
@@ -19,100 +20,186 @@ const About=()=>{
     };
   }, []);
 
-
-return(
-
-<div style={{ fontFamily: 'g-bold'}}>
-
-<div  style={{backgroundColor:"black", fontFamily: 'Gillroy, sans-serif', fontWeight: 800}}class="our container-fluid">
-  <h1 style={{textAlign:"center",paddingTop:"40px",paddingBottom:"40px",color:'#FC772A',fontWeight:"500",fontSize:isSmallScreen?"20px":"38px",fontFamily:"DM Sans"}}> We are Serious about your savings plan</h1>
-  <div style={{
-
-
-
-display:"flex",
-justifyContent:"center",
-margin: "auto",
-  width: "80%",
-   backgroundColor:"black",
-   
-  }} className=" container">
-    <div style={{
-      display:"flex",
-justifyContent:"center",
-    }} class="row">
-      <div style={{padding:"20px"}} class="col-lg-4 col-sm-7" >
-           
-      <Grid  sx={{backgroundColor:"#808080",borderRadius:5,border:" 1px solid #FC772A",marginBottom:"40px"}} container spacing={2}>
-  <Grid style={{borderRadius:10}} item xs={3}>
-   <img style={{height:'80px',width:"80px"}} src="9.png"></img>
-  </Grid>
-  <Grid item xs={8} >
-  <p style={{fontSize:"18px",paddingLeft:"10px",fontWeight:"bold",color:"white",fontFamily:"g-bold",paddingTop:"8px"}}>Max Trust
-              <div>
-
-              </div>
-
-                <span style={{fontSize:"14px",paddingBottom:"15px",fontWeight:"bold",color:"white",fontFamily:"g-light",marginBottom:"10px"}}> Your payment is directly paid to the Jewellery store</span> 
-             </p>
-
-        
-  </Grid>
-</Grid>
-
+  return (
+    <div style={{ fontFamily: 'g-bold' }}>
+      <div
+        style={{
+          backgroundColor: 'black',
+          fontFamily: 'Gillroy, sans-serif',
+          fontWeight: 800
+        }}
+        className="our container-fluid"
+      >
+        <h1
+          style={{
+            textAlign: 'center',
+            paddingTop: '40px',
+            paddingBottom: '40px',
+            color: '#FC772A',
+            fontWeight: '500',
+            fontSize: isSmallScreen ? '20px' : '38px',
+            fontFamily: 'DM Sans'
+          }}
+        >
+          We are Serious about your savings plan
+        </h1>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            margin: 'auto',
+            width: '80%',
+            backgroundColor: 'black'
+          }}
+          className="container"
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Grid
+                sx={{
+                  backgroundColor: '#808080',
+                  borderRadius: 5,
+                  border: '1px solid #FC772A',
+                  marginBottom: '40px'
+                }}
+                container
+                spacing={2}
+              >
+                <Grid item xs={3}>
+                  <img
+                    style={{ height: '80px', width: '80px' }}
+                    src="9.png"
+                    alt="Max Trust"
+                  />
+                </Grid>
+                <Grid item xs={9}>
+                  <p
+                    style={{
+                      fontSize: '18px',
+                      paddingLeft: '10px',
+                      fontWeight: 'bold',
+                      color: 'white',
+                      fontFamily: 'g-bold',
+                      paddingTop: '8px'
+                    }}
+                  >
+                    Max Trust
+                    <div></div>
+                    <span
+                      style={{
+                        fontSize: '14px',
+                        paddingBottom: '15px',
+                        fontWeight: 'bold',
+                        color: 'white',
+                        fontFamily: 'g-light',
+                        marginBottom: '10px'
+                      }}
+                    >
+                      Your payment is directly paid to the Jewellery store
+                    </span>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Grid
+                sx={{
+                  backgroundColor: '#808080',
+                  borderRadius: 5,
+                  border: '1px solid #FC772A',
+                  marginBottom: '40px'
+                }}
+                container
+                spacing={2}
+              >
+                <Grid item xs={3}>
+                  <img
+                    style={{ height: '80px', width: '80px' }}
+                    src="02.png"
+                    alt="Max Safety"
+                  />
+                </Grid>
+                <Grid item xs={9}>
+                  <p
+                    style={{
+                      fontSize: '18px',
+                      paddingLeft: '10px',
+                      fontWeight: 'bold',
+                      color: 'white',
+                      fontFamily: 'g-bold',
+                      paddingTop: '8px'
+                    }}
+                  >
+                    Max Safety
+                    <div></div>
+                    <span
+                      style={{
+                        fontSize: '14px',
+                        paddingBottom: '15px',
+                        fontWeight: 'bold',
+                        color: 'white',
+                        fontFamily: 'g-light',
+                        marginBottom: '10px'
+                      }}
+                    >
+                      Every installment you paid till now is ensured
+                    </span>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Grid
+                sx={{
+                  backgroundColor: '#808080',
+                  borderRadius: 5,
+                  border: '1px solid #FC772A',
+                  marginBottom: '40px'
+                }}
+                container
+                spacing={2}
+              >
+                <Grid item xs={3}>
+                  <img
+                    style={{ height: '80px', width: '80px' }}
+                    src="03.png"
+                    alt="Max Security"
+                  />
+                </Grid>
+                <Grid item xs={9}>
+                  <p
+                    style={{
+                      fontSize: '18px',
+                      paddingLeft: '10px',
+                      fontWeight: 'bold',
+                      color: 'white',
+                      fontFamily: 'g-bold',
+                      paddingTop: '8px'
+                    }}
+                  >
+                    Max Security
+                    <div></div>
+                    <span
+                      style={{
+                        fontSize: '14px',
+                        paddingBottom: '15px',
+                        fontWeight: 'bold',
+                        color: 'white',
+                        fontFamily: 'g-light',
+                        marginBottom: '10px'
+                      }}
+                    >
+                      Means bank graded security and ISO compliance
+                    </span>
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </div>
       </div>
-      <div  style={{padding:"20px"}}class="col-lg-4 col-sm-7 ">
-           
-           <Grid  sx={{backgroundColor:"#808080",borderRadius:5,border:" 1px solid #FC772A",marginBottom:"40px"}} container spacing={2}>
-       <Grid style={{borderRadius:10}} item xs={3}>
-        <img style={{height:'80px',width:"80px"}} src="02.png"></img>
-       </Grid>
-       <Grid item xs={8}>
-
-       <p style={{fontSize:"18px",paddingLeft:"10px",fontWeight:"bold",color:"white",fontFamily:"g-bold",paddingTop:"8px"}}>Max Safety
-              <div>
-
-              </div>
-
-                <span style={{fontSize:"14px",paddingBottom:"15px",fontWeight:"bold",color:"white",fontFamily:"g-light",marginBottom:"10px",marginBottom:"20px"}}> Every installment you paid till now is ensured</span>  
-             </p>
-
-             
-             
-       </Grid>
-     </Grid>
-     
-           </div>
-           <div  style={{padding:"20px",}}class="col-lg-4 col-sm-7 ">
-           
-           <Grid  sx={{backgroundColor:"#808080",borderRadius:5,border:" 1px solid #FC772A",marginBottom:"40px"}} container spacing={2}>
-       <Grid style={{borderRadius:10}} item xs={3}>
-        <img style={{height:'80px',width:"80px"}} src="03.png"></img>
-       </Grid>
-       <Grid item xs={8}>
-             <p style={{fontSize:"18px",paddingLeft:"10px",fontWeight:"bold",color:"white",fontFamily:"g-bold",paddingTop:"8px"}}>Max Security
-              <div>
-
-              </div>
-
-                <span style={{fontSize:"14px",paddingBottom:"15px",fontWeight:"bold",color:"white",fontFamily:"g-light",marginBottom:"30px"}}>    Means bank graded security and iso complainces</span>  
-             </p>
-          
-             
-       </Grid>
-     </Grid>
-     
-           </div>
-
-
     </div>
-  </div>
-  </div>
-</div>
-
-
-)
-
-}
+  );
+};
 
 export default About;
